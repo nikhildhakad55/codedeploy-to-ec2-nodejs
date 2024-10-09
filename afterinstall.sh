70ed6a399471c9657e8d7b8f97b020bbfa52ab04
nikhildhakad55 <153488703+nikhildhakad55@users.noreply.github.com>
2024-10-09 23:30:03 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu
+cd /home/ubuntu/frontend
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 533267021347.dkr.ecr.ap-south-1.amazonaws.com
+docker-compose down
