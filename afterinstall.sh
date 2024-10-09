035185846cefab5d0527a1dc3aa7d01ba2656b00
nikhildhakad55 <153488703+nikhildhakad55@users.noreply.github.com>
2024-10-09 22:55:08 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ubuntu
-npm -f install
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 533267021347.dkr.ecr.ap-south-1.amazonaws.com
