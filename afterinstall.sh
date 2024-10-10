7cbe0bbeab127e48d6cbdf35ec7cf2b5567e7ede
nikhildhakad55 <153488703+nikhildhakad55@users.noreply.github.com>
2024-10-10 16:33:36 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd /home/ubuntu/frontend
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 533267021347.dkr.ecr.ap-south-1.amazonaws.com
-export FRONTEND=533267021347.dkr.ecr.ap-south-1.amazonaws.com/test:latest
-docker-compose down
+cd /home/ubuntu
+npm -f install
